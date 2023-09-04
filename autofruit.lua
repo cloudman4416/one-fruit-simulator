55f62f369d3c3ec933bcc9e22ccf11d2ae385fb2
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2023-09-04 18:18:04 +0200

    Update autofruit.lua

--- autofruit.lua
+++ autofruit.lua
@@ -1,30 +1,112 @@
 repeat wait() until game:IsLoaded()
+getgenv().webhook = "https://discord.com/api/webhooks/1141131986249986118/Crd-Q706FgZ0PrYmXq2UKgVsJZdEWcGbFAYdkz24f7UHeUbcCKvRtGNXijRMh9QclSLU" -- your WebHook goes here
+getgenv().skipfruit =   {		-- put '--' in front of the fruit you want to collect
+                        'BaraFruit',
+                        'BarrierFruit',
+                        'BomuFruit',
+                        --'DragonFruit',
+                        'GomuFruit',
+                        'GoroFruit', 
+                        --'GuraFruit',
+                        'HieFruit',
+                        'ItoFruit',
+                        'KiloFruit',
+                        'LightFruit',
+                        'LoveFruit',
+                        'MagmaFruit',
+                        'MeraFruit',
+                        'MirrorFruit',
+                        --'MochiFruit',
+                        --'OpeFruit',
+                        'PawFruit',
+                        'ShadowFruit',
+                        'SlipFruit',
+                        'SmokeFruit',
+                        --'SoundFruit',
+                        'SpinFruit',
+                        'SpringFruit',
+                        'SukeFruit',
+			            'SunaFruit',
+                        --'VampireFruit',
+                        'YamiFruit',
+                        'YukiFruit',
+                        'ZuchiFrut',
+                        '',}
 local client = game.Players.LocalPlayer
 local HTTPService = game:GetService("HttpService")
 pcall(function()
 repeat wait() until client.PlayerGui.LoadingGui.Frame:FindFirstChild('Indicator')
 end)
 
-function shop()
-    local Http = game:GetService("HttpService")
-    local TPS = game:GetService("TeleportService")
-    local Api = "https://games.roblox.com/v1/games/"
-
-    local _place = game.PlaceId
-    local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
-    function ListServers(cursor)
-    local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-    return Http:JSONDecode(Raw)
-    end
-
-    local Server, Next; repeat
-    local Servers = ListServers(Next)
-    Server = Servers.data[1]
-    Next = Servers.nextPageCursor
-    until Server
-    TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
-
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+local File = pcall(function()
+    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("baboucheserver.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
+    writefile("baboucheserver.json", game:GetService('HttpService'):JSONEncode(AllIDs))
 end
+function TPReturner()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("baboucheserver.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    writefile("baboucheserver.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, client)
+                end)
+                wait(4)
+            end
+        end
+    end
+end
+
+function Teleport()
+    while wait() do
+        pcall(function()
+            TPReturner()
+            if foundAnything ~= "" then
+                TPReturner()
+            end
+        end)
+    end
+end
+
 
 local fruitfound = false
 for i, v in pairs(workspace:GetChildren()) do
@@ -40,7 +122,7 @@ for i, v in pairs(workspace:GetChildren()) do
 end
 
 if not fruitfound then
-    shop()
+    Teleport()
 end
 
 pcall(function()
@@ -156,4 +238,4 @@ wait(1)
 
 
 
-shop()
+Teleport()
